616bf6fb458e8742f50a5e58c0582b5cc66f9cc9
Jezer Mejía <amyuki4@icloud.com>
2022-12-20 22:14:58 -0600

    ¿De dónde vino esto? xd Yeah, removed unnecessary code

--- src/App.vue
+++ src/App.vue
@@ -108,19 +108,6 @@ body {
   margin: 0;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-
 .fade-enter-active,
 .fade-leave-active {
   transition-property: opacity;
--- src/Secondary.vue
+++ src/Secondary.vue
@@ -47,17 +47,4 @@ body {
   padding: 0;
   margin: 0;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
